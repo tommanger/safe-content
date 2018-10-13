87cebf090e2c90de78aeb9dc769a42e380438c33
tom manger <tom.manger10@gmail.com>
2018-10-13 12:14:30 +0300

    update 0.3.1

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ function onLogIn(ev, elForm) {
     var password = elForm.elements.password.value;
     var loggedinUser = checkUserLogin(userName, password)
     if (loggedinUser) {
-        window.location.href = "file:///C:/Users/%D7%AA%D7%95%D7%9D%20%D7%9E%D7%A0%D7%92%D7%A8/Dropbox/CaSep18-ExcerciseSubmission/Tom%20Manger/day%2018/safe-content/main.html";
+        window.location.href = "main.html";
     }
     else {
         document.querySelector('.wrong').style.display = 'block';
